10f324714035c22dc0c9c2f95eaa21547d9743ea
Sirius Deis <falebsy@gmail.com>
2023-04-22 09:29:55 +0200

    add cart model

--- src/models/cart.model.js
+++ src/models/cart.model.js
@@ -1,1 +1,24 @@
-const express = require("express");
+const mongoose = require("mongoose");
+
+const CartSchema = mongoose.Schema({
+    userId: {
+        type: mongoose.Types.ObjectId,
+        ref: "User",
+    },
+    products: [
+        {
+            productId: {
+                type: mongoose.Types.ObjectId,
+                ref: "Product",
+            },
+            quantity: {
+                type: Number,
+                required: [true, "This field can't be blank"],
+            },
+        },
+    ],
+});
+
+const Cart = mongoose.model("Carts", CartSchema);
+
+module.exports = Cart;
