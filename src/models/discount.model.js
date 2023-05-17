190f3310fedcefdfdc7fcd3e6e167f10bddee731
Sirius Deis <falebsy@gmail.com>
2023-05-17 11:17:20 +0200

    add model for discount

---
+++ src/models/discount.model.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+
+const DiscountSchema = new mongoose.Schema({
+    productId: {
+        type: mongoose.SchemaTypes.ObjectId,
+        required: true,
+        ref: 'product',
+    },
+    amount: {
+        type: Number,
+        min: 0,
+        max: 100,
+    },
+    addedAt: {
+        type: Date,
+        default: Date.now(),
+        required: true,
+    },
+    till: {
+        type: Date,
+        required: true,
+    },
+});
+
+const Discount = mongoose.model('discount', DiscountSchema);
+
+module.exports = Discount;
