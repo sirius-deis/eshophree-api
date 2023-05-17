ce02ce2b3f46ce0bfac3d26d2845cb8b6dba575c
Sirius Deis <falebsy@gmail.com>
2023-05-17 11:13:35 +0200

    add model for review

---
+++ src/models/review.model.js
@@ -0,0 +1,35 @@
+const mongoose = require('mongoose');
+
+const ReviewSchema = new mongoose.Schema({
+    userId: {
+        type: mongoose.SchemaTypes.ObjectId,
+        required: true,
+        ref: 'user',
+    },
+    productId: {
+        type: mongoose.SchemaType.ObjectId,
+        required: true,
+        ref: product,
+    },
+    rating: {
+        type: Number,
+        required: true,
+        min: 1,
+        max: 5,
+    },
+    comment: {
+        type: String,
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now(),
+        require: true,
+    },
+    editedAt: {
+        type: Date,
+    },
+});
+
+const Review = mongoose.model('review', ReviewSchema);
+
+module.exports = Review;
