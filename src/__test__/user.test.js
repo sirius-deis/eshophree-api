7a774595a6da33575be43184eb9b4828a454b73b
Sirius Deis <falebsy@gmail.com>
2023-05-26 14:01:35 +0200

    add tests for /login route

--- src/__test__/user.test.js
+++ src/__test__/user.test.js
@@ -7,17 +7,13 @@ describe('/users', () => {
         await connect();
     });
     afterEach(async () => {
-        await clearDatabase();
+        // await clearDatabase();
     });
     afterAll(async () => {
+        await clearDatabase();
         await closeDatabase();
     });
 
-    "Invalid value. Field 'email' with value 'undefined' doesn't pass validation. Please provide correct data",
-        "Invalid value. Field 'name' with value '' doesn't pass validation. Please provide correct data",
-        "Invalid value. Field 'surname' with value '' doesn't pass validation. Please provide correct data",
-        "Invalid value. Field 'password' with value '' doesn't pass validation. Please provide correct data",
-        "Invalid value. Field 'passwordConfirm' with value '' doesn't pass validation. Please provide correct data";
     describe('/signup route', () => {
         it('should return 400 error as body is empty', done => {
             request(app)
@@ -126,7 +122,7 @@ describe('/users', () => {
                 });
         });
 
-        it('should successfully register a new account', done => {
+        it('should return 400 error as body is empty', done => {
             request(app)
                 .post('/api/v1/users/signup')
                 .type('json')
@@ -136,11 +132,169 @@ describe('/users', () => {
                     surname: 'test surname',
                     email: 'test@test.com',
                     password: 'password123',
+                })
+                .expect('Content-Type', /json/)
+                .expect(400)
+                .expect(res => {
+                    expect(res.body.message).toEqual([
+                        "Invalid value. Field 'passwordConfirm' with value '' doesn't pass validation. Please provide correct data",
+                    ]);
+                })
+                .end((err, res) => {
+                    if (err) {
+                        done(err);
+                    } else {
+                        done();
+                    }
+                });
+        });
+
+        it('should return 400 error as passwords are not the same', done => {
+            request(app)
+                .post('/api/v1/users/signup')
+                .type('json')
+                .set('Accept', 'application/json')
+                .send({
+                    name: 'test name',
+                    surname: 'test surname',
+                    email: 'test@test.com',
+                    password: 'password123',
+                    passwordConfirm: 'password',
+                })
+                .expect('Content-Type', /json/)
+                .expect(400)
+                .expect(res => {
+                    expect(res.body.message).toEqual(
+                        'Passwords are not the same. Please provide correct passwords'
+                    );
+                })
+                .end((err, res) => {
+                    if (err) {
+                        done(err);
+                    } else {
+                        done();
+                    }
+                });
+        });
+
+        it('should successfully register a new account', done => {
+            request(app)
+                .post('/api/v1/users/signup')
+                .type('json')
+                .set('Accept', 'application/json')
+                .send({
+                    name: 'test name',
+                    surname: 'test surname',
+                    email: 'test1@test.com',
+                    password: 'password123',
                     passwordConfirm: 'password123',
                 })
                 .expect('Content-Type', /json/)
                 .expect(201)
-                .expect(res => {})
+                .expect(res => {
+                    expect(res.body.message).toBe(
+                        'Account was successfully created'
+                    );
+                })
+                .end((err, res) => {
+                    if (err) {
+                        done(err);
+                    } else {
+                        done();
+                    }
+                });
+        });
+    });
+    describe('/login route', () => {
+        it('should return 400 error as there are not valid fields in body', done => {
+            request(app)
+                .post('/api/v1/users/login')
+                .type('json')
+                .set('Accept', 'application/json')
+                .send({})
+                .expect('Content-Type', /json/)
+                .expect(400)
+                .expect(res => {
+                    expect(res.body.message).toEqual([
+                        "Invalid value. Field 'email' with value 'undefined' doesn't pass validation. Please provide correct data",
+                        "Invalid value. Field 'password' with value '' doesn't pass validation. Please provide correct data",
+                    ]);
+                })
+                .end((err, res) => {
+                    if (err) {
+                        done(err);
+                    } else {
+                        done();
+                    }
+                });
+        });
+        it('should return 401 error as password was incorrect', done => {
+            request(app)
+                .post('/api/v1/users/login')
+                .type('json')
+                .set('Accept', 'application.json')
+                .send({
+                    email: 'test@test.com',
+                    password: 'password1',
+                })
+                .expect('Content-Type', /json/)
+                .expect(401)
+                .expect(res => {
+                    expect(res.body.message).toBe(
+                        'Incorrect email or password'
+                    );
+                })
+                .end((err, res) => {
+                    if (err) {
+                        done(err);
+                    } else {
+                        done();
+                    }
+                });
+        });
+
+        it('should return 401 error as password was incorrect', done => {
+            request(app)
+                .post('/api/v1/users/login')
+                .type('json')
+                .set('Accept', 'application.json')
+                .send({
+                    email: 'test@test.com',
+                    password: 'password1',
+                })
+                .expect('Content-Type', /json/)
+                .expect(401)
+                .expect(res => {
+                    expect(res.body.message).toBe(
+                        'Incorrect email or password'
+                    );
+                })
+                .end((err, res) => {
+                    if (err) {
+                        done(err);
+                    } else {
+                        done();
+                    }
+                });
+        });
+
+        it('should return 200 status after successful logging in', done => {
+            request
+                .agent(app)
+                .post('/api/v1/users/login')
+                .type('json')
+                .set('Accept', 'application.json')
+                .send({
+                    email: 'test1@test.com',
+                    password: 'password123',
+                })
+                .expect('Content-Type', /json/)
+                .expect(200)
+                .expect(res => {
+                    expect(res.body.message).toBe(
+                        'You was sign in successfully'
+                    );
+                })
                 .end((err, res) => {
                     if (err) {
                         done(err);
--- src/controllers/user.controllers.js
+++ src/controllers/user.controllers.js
@@ -91,7 +91,7 @@ exports.login = catchAsync(async (req, res, next) => {
 
     const user = await User.findOne({ email }).select('+password');
     if (!user || !(await user.checkPassword(password, user.password))) {
-        return next(new AppError('Incorrect email or password', 400));
+        return next(new AppError('Incorrect email or password', 401));
     }
     user.password = undefined;
 
