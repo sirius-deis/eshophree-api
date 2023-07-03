6385b2ec2d1f6c13b61958413c5fd9e16d4a0363
Sirius Deis <falebsy@gmail.com>
2023-07-03 07:16:49 +0200

    add controller for deleting product from wishlist

--- src/controllers/wishlist.controllers.js
+++ src/controllers/wishlist.controllers.js
@@ -1,15 +1,9 @@
 const catchAsync = require('../utils/catchAsync');
 const AppError = require('../utils/appError');
 const Wishlist = require('../models/wishlist.models');
-const Product = require('../models/product.models');
 
 exports.addToWishlist = catchAsync(async (req, res, next) => {
-  const { user } = req;
-  const { productId } = req.params;
-  const product = await Product.findById(productId);
-  if (!product) {
-    return next(new AppError('There is no product with such id', 404));
-  }
+  const { user, product } = req;
   let wishlist = await Wishlist.findOne({
     userId: user._id,
   });
@@ -18,17 +12,33 @@ exports.addToWishlist = catchAsync(async (req, res, next) => {
     wishlist = await Wishlist.create({ userId: user._id });
   }
 
-  const productInWishlist = wishlist.products.find((prod) => prod.productId.equals(productId));
+  const productInWishlist = wishlist.products.find((prod) => prod.productId.equals(product._id));
 
   if (productInWishlist) {
     return next(new AppError('This product is already in wishlist', 400));
   }
 
-  wishlist.products.push({ productId });
+  wishlist.products.push({ productId: product._id });
 
   await wishlist.save();
 
   return res.status(200).json({ message: 'Product was added to your wishlist successfully' });
 });
 
-exports.deleteFromWishlist = catchAsync(async (req, res, next) => {});
+exports.deleteFromWishlist = catchAsync(async (req, res, next) => {
+  const { user, product } = req;
+
+  const wishlist = await Wishlist.findOne({
+    userId: user._id,
+  });
+
+  if (!wishlist) {
+    return next(new AppError('There is no wishlist for selected user', 404));
+  }
+
+  wishlist.products = wishlist.products.filter((prod) => !prod.productId.equals(product._id));
+
+  await wishlist.save();
+
+  return res.status(200).json({ message: 'Product was deleted from your wishlist successfully' });
+});
--- src/routes/wishlist.routes.js
+++ src/routes/wishlist.routes.js
@@ -1,11 +1,13 @@
 const express = require('express');
-const { addToWishlist } = require('../controllers/wishlist.controllers');
+const { addToWishlist, deleteFromWishlist } = require('../controllers/wishlist.controllers');
 const { isLoggedIn } = require('../middlewares/auth.middlewares');
+const { findProduct } = require('../middlewares/product.middlewares');
 
 const wishlistRouter = express.Router({ mergeParams: true });
 
 wishlistRouter.use(isLoggedIn);
+wishlistRouter.use(findProduct);
 
-wishlistRouter.route('/').patch(addToWishlist);
+wishlistRouter.route('/').patch(addToWishlist).delete(deleteFromWishlist);
 
 module.exports = wishlistRouter;
